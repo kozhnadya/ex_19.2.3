66849d95c8b125d199095a3710d177657d4a80ba
unknown <kozh.nadya@gmail.com>
2022-06-03 12:18:50 -0700

    Signed-off-by: unknown <kozh.nadya@gmail.com>

---
+++ app/calculator.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  3 09:36:46 2022
+
+@author: zbookuser
+"""
+
+class Calculator:
+   def multiply(self, x, y):
+       return x * y
+
+   def division(self, x, y):
+       return x / y
+
+   def subtraction(self, x, y):
+       return x - y
+
+   def adding(self, x, y):
+       return x + y
+   
